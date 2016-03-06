101df017559bbf657abd5f1b7fb51473392feb40
Your Name <you@example.com>
2016-03-06 07:15:42 +0100

    fghgfhgfh

--- test.php
+++ test.php
@@ -1,7 +1,20 @@
 fghghgfhgf
 ewrewrwr
 hggjgj
+uyiuyiuui
+uyiuyiuuiui
+yu
+iuy
 
+<<<<<<< f35ff330fb8b3a1257e7fa5abef923f75c1ecb24
+=======
+ghj
+ghj
+hgj
+
+1
+2
+>>>>>>> 1555a751fed3777209b1c8d3466ea4fb5e10c7a3
 3
 4
 5
