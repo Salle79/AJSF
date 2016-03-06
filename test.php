d6166573083a83bb6d2332560c5922b730eef260
Your Name <you@example.com>
2016-03-06 07:17:58 +0100

    fghgfhgfh (reverted from commit 1555a751fed3777209b1c8d3466ea4fb5e10c7a3)

--- test.php
+++ test.php
@@ -1,23 +1,9 @@
-fghghgfhgf
-ewrewrwr
-hggjgj
-uyiuyiuui
-uyiuyiuuiui
-yu
-iuy
-
-<<<<<<< f35ff330fb8b3a1257e7fa5abef923f75c1ecb24
-=======
-ghj
-ghj
-hgj
-
 1
 2
->>>>>>> 1555a751fed3777209b1c8d3466ea4fb5e10c7a3
 3
 4
 5
 6
 7
-hgj
+8
+9
